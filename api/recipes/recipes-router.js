182c619384241515709a4bf00bbb5a4f16904ea1
djho57 <daniel.j.ho57@gmail.com>
2021-12-10 01:30:29 -0600

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,4 +1,13 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
+
+
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+})
 
 router.use('*', (req, res, next) => { //eslint-disable-line
     res.json({ api: 'up' })
